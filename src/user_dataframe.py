84529f9d22c5d5d33ad99dd91f0e1f6b022cb5e6
Sim Borodach <sborodach@protonmail.com>
2021-07-15 12:25:29 -0400

    user dataframe updates

--- src/user_dataframe.py
+++ src/user_dataframe.py
@@ -16,27 +16,37 @@ class UserDataFrameWrangler():
     def __init__(self, user_dataframe):
         self.user_dataframe = user_dataframe
         self.impression_news = {}
-        self.target_news = {}
+        self.target_articles = {}
         self.impression = []
+        self.clean_user_dataframe()
 
     def clean_user_dataframe(self):
+        self._name_and_drop_columns()
+
+    def _name_and_drop_columns(self):
         self.user_dataframe.drop([0], axis=1, inplace=True)
         self.user_dataframe.columns = ['Impression_ID', 'User_ID', 'Timestamp', 'History', 'Impressions']
-        self.user_dataframe.drop(['Impression_ID', 'Timestamp', 'Impressions'], axis=1, inplace=True)
+        self.user_dataframe.drop(['Impression_ID', 'Timestamp'], axis=1, inplace=True)
+
+    def _clean_history_column(self):
         self.user_dataframe.drop(self.user_dataframe[self.user_dataframe['History'].isna() == True].index, axis=0, inplace=True)
         self.user_dataframe['History'] = self.user_dataframe['History'].apply(lambda x: x.split(' '))
+
+    def _get_impressions_and_target_articles(self):
         impressions = self.user_dataframe['Impressions'].apply(lambda x: x.split(' '))
         for i, impression in enumerate(impressions):
             self.impression_news[impressions.index[i]] = []
             for article in impression:
                 self.impression_news[impressions.index[i]].append(article[:-2])
                 if '-1' in article:
-                    self.target_news[impressions.index[i]] = article[:-2]
+                    self.target_articles[impressions.index[i]] = article[:-2]
 
+    def add_impressions_and_target_column_values(self):
         self.user_dataframe['Impressions'] = self.impression_news.values()
-        self.user_dataframe['Target'] = self.target_news.values()
+        self.user_dataframe['Target'] = self.target_articles.values()
 
 if  __name__ == "__main__":
-    userDataFrameProcessor = UserDataFrameProcessor("../data/behaviors_test.tsv")
+    behaviors_tsv_filepath = "../data/beahviors_test.tsv"
+    userDataFrameProcessor = UserDataFrameProcessor(behaviors_tsv_filepath)
     userDataFrameProcessor.clean_user_dataframe()
     print(userDataFrameProcessor.user_dataframe.head())
